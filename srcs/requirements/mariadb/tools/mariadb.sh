ee892bacfceb5b1157d06c14570d9e3e7a7beb71
tayqiter00 <tayqiter00@gmail.com>
2024-12-02 22:15:42 +0800

    mariadb doesn't seem to launch as root so tried service mysql start

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# & runs it as a background first
-mariadbd --skip-networking &
+service mysql start 
 
-until mariadb -u root -e "SELECT 1;" &> /dev/null; do
-    sleep 1
-done
-
-mariadb -u root << EOF
+mysql << EOF
 CREATE DATABASE IF NOT EXISTS ${wp_database};
 CREATE USER IF NOT EXISTS '${mariadb_user}'@'localhost' IDENTIFIED BY '${mariadb_pwd}';
 GRANT ALL PRIVILEGES ON ${wp_database}.* TO '${mariadb_user}'@'localhost';
@@ -15,6 +10,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${mariadb_root_pwd}';
 FLUSH PRIVILEGES;
 EOF
 
-killall mariadbd
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-exec mariadbd
+mysqld
