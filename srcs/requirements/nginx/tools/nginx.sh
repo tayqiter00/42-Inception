05bbae91fec7e0dfa34a95530a8358efe767b0ee
tayqiter00 <tayqiter00@gmail.com>
2024-12-02 22:14:21 +0800

    Comment out chmod 600 for the private key + misc. formatting

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/certs/nginx-certificate.crt -keyout /etc/ssl/private/nginx.key -subj "/C=MY/ST=Selangor/L=Petaling Jaya/O=42KL/CN=qtay/"
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 \
+-nodes -out /etc/ssl/certs/nginx-certificate.crt \
+-keyout /etc/ssl/private/nginx.key \
+-subj "/C=MY/ST=Selangor/L=Petaling Jaya/O=42KL/CN=qtay/"
 
-chmod 600 /etc/ssl/private/nginx.key
+# chmod 600 /etc/ssl/private/nginx.key
 
 nginx -g "daemon off"
